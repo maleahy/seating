45d53dca6e6fefc14dd41b0f02cd679120b1eeca
maleahy <maleahyee@gmail.com>
2026-04-17 19:56:19 -0600

    fix: move table 2 up in seating diagram

--- docs/app.js
+++ docs/app.js
@@ -41,7 +41,7 @@ function renderSeatingDiagram(highlightTable) {
   const tables = [
     // Right column: tables 1-2, portrait orientation (rotated 90°)
     [1, 330, 133, 38, 78],  // bottom
-    [2, 330,  38, 38, 78],  // top
+    [2, 330,  0, 38, 78],  // top
     // Grid bottom row (tables 3-6), flush horizontally
     [3,   8, 203, 78, 38],
     [4,  86, 203, 78, 38],
